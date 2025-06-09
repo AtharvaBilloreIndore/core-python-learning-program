11c3b165c93034f134196da752617d260a605aff
Atharva Billore <billoreatharva22@gmail.com>
2025-06-09 12:11:40 +0530

    Data Processor

--- beginner/io_bound_ops.py
+++ beginner/io_bound_ops.py
@@ -17,50 +17,50 @@ The examples in this file will demonstrate how to handle I/O operations efficien
 """
 
 # TODO: Add I/O bound operations examples and implementations
-# from datetime import datetime
-# def create_entry():
-#     entry = input("Enter your journal entry: ")
-#     date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     with open("journal.txt", "a") as file:
-#         file.write(f"\n{entry} {date_str}")
-#     print("Entry saved!\n")
+from datetime import datetime
+def create_entry():
+    entry = input("Enter your journal entry: ")
+    date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("journal.txt", "a") as file:
+        file.write(f"\n{entry} {date_str}")
+    print("Entry saved!\n")
     
-# def read_entries():
-#     try:
-#         with open("journal.txt", "r") as file:
-#             content = file.read()
-#             print("\nJournal Entries\n")
-#             print(content)
-#     except FileNotFoundError:
-#         print("No journal entries found yet.\n")
+def read_entries():
+    try:
+        with open("journal.txt", "r") as file:
+            content = file.read()
+            print("\nJournal Entries\n")
+            print(content)
+    except FileNotFoundError:
+        print("No journal entries found yet.\n")
         
-# def main():
-#     try:
-#         flag = True
-#         while flag:
-#             print("1. Write a new entry")
-#             print("2. Read past entries")
-#             print("3. Exit")
-#             choice = int(input("Choose an option: "))
-#             if choice not in [1, 2, 3]:
-#                 raise ValueError("Choice out of valid range.")
-#             if choice == 1:
-#                 create_entry()
-#             elif choice == 2:
-#                 read_entries()
-#             elif choice == 3:
-#                 print("Goodbye!")
-#                 flag = False
-#                 break
-#             else:
-#                 print("Invalid choice. Try again.\n")      
-#     except ValueError as e:
-#             print(f"Invalid input: {e}")
-#             return
-#     except Exception as e:
-#         print(f"An unexpected error occurred: {e}")
-#         return
-# main()
+def main():
+    try:
+        flag = True
+        while flag:
+            print("1. Write a new entry")
+            print("2. Read past entries")
+            print("3. Exit")
+            choice = int(input("Choose an option: "))
+            if choice not in [1, 2, 3]:
+                raise ValueError("Choice out of valid range.")
+            if choice == 1:
+                create_entry()
+            elif choice == 2:
+                read_entries()
+            elif choice == 3:
+                print("Goodbye!")
+                flag = False
+                break
+            else:
+                print("Invalid choice. Try again.\n")      
+    except ValueError as e:
+            print(f"Invalid input: {e}")
+            return
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return
+main()
 
 import csv
 import json
